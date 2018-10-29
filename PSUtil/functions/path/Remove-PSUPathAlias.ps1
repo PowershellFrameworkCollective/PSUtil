42ebbcbc5c5db182dceade8c2fdcea08e3c3a1ac
Andrew Pla <32366464+AndrewPla@users.noreply.github.com>
2018-10-28 22:40:19 -0400

    added remove-psupathalias

--- PSUtil/functions/path/Remove-PSUPathAlias.ps1
+++ PSUtil/functions/path/Remove-PSUPathAlias.ps1
@@ -1,11 +1,12 @@
-function Remove-PSUPathAlias {
+function Unregister-PSUPathAlias {
     <#
     .SYNOPSIS
-        Removes a path alias form the configuration system
+        Removes a path alias fromm the configuration system.
     .DESCRIPTION
-        Removes a path alias form the configuration system
+        Removes a path alias from the configuration system using Unregister-PSFConfig.
+        Note: This command has no effect on configuration setings currently in memory.
     .PARAMETER Alias
-        The Alias that you want to remove.
+        The name of the Alias that you want to remove from the configuration system.
     .EXAMPLE
         PS C:\> Remove-PSUPathAlias -Alias work
         Removes the path alias named work from the configuration system.
@@ -14,5 +15,5 @@ function Remove-PSUPathAlias {
     param (
         $Alias
     )
-    #TODO write this command
+    Get-PSFConfig -FullName psutil.pathalias.$Alias | Unregister-PSFConfig
 }
