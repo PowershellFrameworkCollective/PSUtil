f0bd43fdfc2ed57150572904c694bba19fecc86a
AndrewPla <32366464+AndrewPla@users.noreply.github.com>
2018-11-23 11:10:55 -0500

    added -notnull , updated errors, removed SB support, added env: support

--- PSUtil/functions/path/Set-PSUPath.ps1
+++ PSUtil/functions/path/Set-PSUPath.ps1
@@ -9,7 +9,11 @@
 
 	.PARAMETER Alias
 		This is the name of the alias that called the command.
-		Default Value is $MyInvocation.line and is detected automatically
+        Default Value is $MyInvocation.line and is detected automatically
+
+    .PARAMETER EnableException
+        Replaces user friendly yellow warnings with bloody red exceptions of doom!
+        Use this if you want the function to throw terminating errors you want to catch.
 
 	.EXAMPLE
 		PS C:\> Software
@@ -22,32 +26,43 @@
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseShouldProcessForStateChangingFunctions", "")]
     [CmdletBinding()]
     param (
-        $Alias = $MyInvocation.line
+        $Alias = $MyInvocation.line,
+
+        [switch]
+        $EnableException
     )
 
     Write-PSFMessage -Level InternalComment -Message "Bound parameters: $($PSBoundParameters.Keys -join ", ")" -Tag 'debug', 'start', 'param'
     try {
-        $PSFConfigPath = Get-PSFConfigValue -FullName psutil.pathalias.$Alias
-        # Turn Value into scriptblock to support Variables and $env:
-        if ($PSFConfigPath -contains '$') {
-            $ScriptBlock = [scriptblock]::Create($PSFConfigPath)
-        }
+        $PSFConfigPath = Get-PSFConfigValue -FullName psutil.pathalias.$Alias -NotNull
     }
 
     catch {
-        Stop-PSFFunction -Message "unable to find PSFConfig entry for psutil.pathalias.$Alias" -Category InvalidOperation -Tag fail -Exception $_
+        Stop-PSFFunction -Message "Unable to find a path setting for the alias" -Category InvalidOperation -Tag fail -Exception $_
         return
     }
+
     try {
-        if ($ScriptBlock) {
-            Set-Location (& $ScriptBlock)
+
+        if ($PSFConfigPath -contains '$env:') {
+            Write-PSFMessage "Environmental variable detected, resolving path" -Level internalcomment
+            Set-Location (Resolve-Path $PSFConfigPath)
         }
+
         else {
             Set-Location $PSFConfigPath
         }
     }
     catch {
-        Stop-PSFFunction -Message "Unable to set location to $PSFConfigPath" -Category InvalidOperation -Tag fail -Exception $_
+
+        $psfFuncParams = @{
+            Message         = "Unable to set location to $PSFConfigPath"
+            Category        = 'InvalidOperation'
+            Tag             = 'fail'
+            ErrorRecord     = $_
+            EnableException = $EnableExceiption
+        }
+        Stop-PSFFunction @psfFuncParams
         return
     }
 }
