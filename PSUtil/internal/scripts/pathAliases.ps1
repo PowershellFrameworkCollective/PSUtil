f21e0817813df524fd848997bc214cfa8f5406b1
AndrewPla <32366464+AndrewPla@users.noreply.github.com>
2018-11-23 11:22:15 -0500

    syntax improvement.

--- PSUtil/internal/scripts/pathAliases.ps1
+++ PSUtil/internal/scripts/pathAliases.ps1
@@ -1,5 +1,5 @@
 ﻿# Set Path Aliases for Set-PSUPath from config system
-$aliases = ((Get-PSUPathAlias).name.replace('.', ' ') -split ' ')[-1]
+$aliases = ((Get-PSUPathAlias).name -split '\.')[-1]
 
 foreach ($alias in $aliases) {
     Import-PSUAlias -Name $alias -Command 'Set-PSUPath'
