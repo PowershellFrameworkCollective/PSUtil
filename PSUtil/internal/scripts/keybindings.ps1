3781081720841f018ab31282b1e92b6aa2a4de0d
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2018-12-16 09:27:55 +0100

    fixed legacy variable name

--- PSUtil/internal/scripts/keybindings.ps1
+++ PSUtil/internal/scripts/keybindings.ps1
@@ -1,6 +1,6 @@
 ﻿if ((Get-PSFConfigValue -FullName "PSUtil.Import.Keybindings" -Fallback $true) -and (Get-Module PSReadline))
 {
-	foreach ($file in (Get-ChildItem -Path (Join-PSFPath $script:PSModuleRoot 'internal' 'keybindings')))
+	foreach ($file in (Get-ChildItem -Path (Join-PSFPath $script:ModuleRoot 'internal' 'keybindings')))
 	{
 		. Import-ModuleFile -Path $file.FullName
 	}
